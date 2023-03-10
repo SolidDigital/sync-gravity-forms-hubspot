69958c6bc8496eb87ff1020e7feb8500a9759071
Cooper Hilscher <cooper.hilscher@soliddigital.com>
2023-03-10 15:40:15 -0800

    Initial commit

---
+++ gf-hubspot.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name:  GF HubSpot Plugin
+Plugin URI:   TBD 
+Description:  Wordpress plugin that integrates HubSpot with Gravity Forms 
+Version:      1.0
+Author:       Solid Digital 
+Author URI:   https://www.soliddigital.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  gfhubspot
+*/
+
+define( 'GF_HUBSPOT', __FILE__ );
+require plugin_dir_path( GF_HUBSPOT ) . 'class-gf-hubspot.php';
